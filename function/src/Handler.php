ec05aa77c13734a1ef4dd7fa0bb60e7dbcf3c8c1
Matt Withoos <mattwithoos@gmail.com>
2022-01-24 00:48:52 +1100

    first commit

---
+++ function/src/Handler.php
@@ -0,0 +1,13 @@
+<?php
+
+declare('strict_types=1');
+
+namespace App;
+
+class Handler
+{
+    public function handle(string $data): string
+    {
+        return $data;
+    }
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare('strict_types=1');
+
+// Requires Function composer's autoload
+if (file_exists('function/vendor/autoload.php')) {
+    require('function/vendor/autoload.php');
+}
+
+$stdin = file_get_contents("php://stdin");
+$response = (new App\Handler())->handle($stdin);
+echo $response;
